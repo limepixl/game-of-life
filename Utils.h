bb1b38d4c85d2fa9f1ebf065635d74e8bb5a75c1
Stefan Ivanovski <stefs.ivanovski@gmail.com>
2018-11-15 20:46:25 +0100

    Cleaned up code for better readibility

--- Utils.h
+++ Utils.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <vector>
 #include <time.h>
 #include "RectangleShape.h"
@@ -10,10 +9,10 @@ private:
 
 public:
     // Check the neighbours of a cell and add up the number of alive cells
-    static int checkCell(std::vector<RectangleShape> neighbours) {
+    static int checkCell(std::vector<RectangleShape>& neighbours) {
         int sum = 0;
-        for(int i = 0; i < neighbours.size(); i++) { 
-            if(neighbours.at(i).value) {
+        for(unsigned int i = 0; i < neighbours.size(); i++) { 
+            if(neighbours[i].getFillColor() == sf::Color(255,255,255,255)) {
                 sum++;
             }
         }
@@ -23,7 +22,7 @@ public:
     // Generate an array of rectangle shapes with randomized values.
     // n - number of rows and columns
     // width / height - the dimensions of the window
-    static std::vector<std::vector<RectangleShape>> genArray(int n, unsigned int width, unsigned int height) {
+    static void genArray(std::vector<std::vector<RectangleShape>>& grid, int n, unsigned int width, unsigned int height) {
         std::srand(time(nullptr));
 
         std::vector<std::vector<RectangleShape>> rows;
@@ -45,49 +44,37 @@ public:
             }
             rows.push_back(row);
         }
-
-        return rows;
     }
 
     // Check each neighbour to see if it will die or live in the next generation
-    static std::vector<std::vector<RectangleShape>> checkNeighbours(std::vector<std::vector<RectangleShape>> grid, int n) {
-        std::vector<std::vector<RectangleShape>> next;
-        next = grid;
-
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid.size(); j++) {
-                // Store all neighbours of each cell in a vector
-                std::vector<RectangleShape> neighbours;
-                for(int k = -1; k < 2; k++) {
-                    for(int l = -1; l < 2; l++) {
-                        if(!(k == 0 && l == 0))
-                            neighbours.push_back(grid.at((i+k+n)%n).at((j+l+n)%n));
-                    }
-                }
-
-                // Check neighbours of each cell
-                int sum = checkCell(neighbours);
-                if(sum < 2 || sum > 3)
-                    next.at(i).at(j).value = false;  
-                else if(sum == 3 && !grid.at(i).at(j).value)
-                    next.at(i).at(j).value = true;
+    static void checkNeighbours(std::vector<std::vector<RectangleShape>>& grid, int n) {
+        for(unsigned int i = 0; i < grid.size(); i++)
+        for(unsigned int j = 0; j < grid.size(); j++) {
+            // Store all neighbours of each cell in a vector
+            std::vector<RectangleShape> neighbours;
+            for(int k = -1; k < 2; k++)
+            for(int l = -1; l < 2; l++) {
+                    if(!(k == 0 && l == 0))
+                        neighbours.push_back(grid.at((i+k+n)%n).at((j+l+n)%n));
             }
-        }
 
-        return next;
+            // Check neighbours of each cell
+            int sum = checkCell(neighbours);
+            if(sum < 2 || sum > 3)
+                grid[i][j].value = false;  
+            else if(sum == 3 && !grid[i][j].value)
+                grid[i][j].value = true;
+        }
     }
 
     // Update vector entries (rectangle shapes) to update the fill color of each cell
-    static std::vector<std::vector<RectangleShape>> updateVector(std::vector<std::vector<RectangleShape>> grid) {
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid.size(); j++) {
-                if(grid.at(i).at(j).value)
-                    grid.at(i).at(j).setFillColor(sf::Color(255,255,255,255));
+    static void updateVector(std::vector<std::vector<RectangleShape>>& grid) {
+        for(unsigned int i = 0; i < grid.size(); i++)
+        for(unsigned int j = 0; j < grid.size(); j++) {
+                if(grid[i][j].value)
+                    grid[i][j].setFillColor(sf::Color(255,255,255,255));
                 else  
-                    grid.at(i).at(j).setFillColor(sf::Color(0,0,0,255));                    
-            }
+                    grid[i][j].setFillColor(sf::Color(0,0,0,255));                    
         }
-
-        return grid;
     }
 };
--- main.cpp
+++ main.cpp
@@ -3,41 +3,37 @@
 #include "Utils.h"
 
 int main() {
-    // Constants
-    const unsigned int WIDTH = 600;
-    const unsigned int HEIGHT = 600;
-    const int n = 100;
+	// Constants
+	const unsigned int WIDTH = 600;
+	const unsigned int HEIGHT = 600;
+	const int n = 100;
 
-    // Create render window
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Game of Life");
+	// Create render window
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Game of Life");
 
-    // Store randomly generated dead and alive cells in 2D vector
-    std::vector<std::vector<RectangleShape>> grid = Utils::genArray(n, WIDTH, HEIGHT);
+	std::vector<std::vector<RectangleShape>> grid;
 
-    // Render loop
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	// Store randomly generated dead and alive cells in 2D vector
+	Utils::genArray(grid, n, WIDTH, HEIGHT); 
 
-        std::vector<std::vector<RectangleShape>> next = Utils::checkNeighbours(grid, n);
-        
-        next = Utils::updateVector(next);
+	// Render loop
+	while(window.isOpen()) {
+		sf::Event event;
+		while(window.pollEvent(event)) {
+			if(event.type == sf::Event::Closed)
+				window.close();
+		}
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                window.draw(grid.at(i).at(j));
-            }
-        }
+		Utils::checkNeighbours(grid, n);
+		Utils::updateVector(grid);
 
-        grid = next;
+		for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++) {
+			window.draw(grid[i][j]);
+		}
 
-        window.display();
-    }
+		window.display();
+	}
 
-    return 0;
+	return 0;
 }
