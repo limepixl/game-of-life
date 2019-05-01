0a283eadab38f19c7ee0e569a0f9f0b67267bdf3
Stefan Ivanovski <stefs.ivanovski@gmail.com>
2019-05-01 12:34:31 +0200

    Major overhaul and performance gains.

--- main.cpp
+++ main.cpp
@@ -1,127 +1,98 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <ctime>
 
-enum State
+int countNeighbors(bool grid[], int index, int rows, int cols)
 {
-	DEAD,
-	ALIVE
-};
+	/*
+	bool tl = grid[index - cols - 1];
+	bool t =  grid[index - cols];
+	bool tr = grid[index - cols + 1];
+	bool l =  grid[index - 1];
+	bool r =  grid[index + 1];
+	bool bl = grid[index + cols - 1];
+	bool b =  grid[index + cols];
+	bool br = grid[index + cols + 1];
+	*/
 
-void genArray(std::vector<State>& grid, int rows, int cols);
-
-int checkCell(std::vector<State>& neighbours);
-
-void checkNeighbours(std::vector<State>& grid, std::vector<State>& next, int rows, int cols);
-
-int main()
-{
-	// Constants
-	const int WIDTH = 1280;
-	const int HEIGHT = 720;
-	const int size = 5;
-	const int rows = WIDTH / size;	// Number of cells in each row
-	const int cols = HEIGHT / size;	// Number of cells in each collumn
-
-	// Create render window
-	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Game of Life");
-	window.setFramerateLimit(60);
-
-	std::vector<sf::RectangleShape> cells;
-	std::vector<State> states;
-
-	for(int i = 0; i < rows; i++)
-	for(int j = 0; j < cols; j++)
-	{
-		sf::RectangleShape cell(sf::Vector2f(size, size));
-		cell.setPosition(sf::Vector2f((float)size * i, (float)size * j));
-		cell.setFillColor(sf::Color(255, 255, 255, 255));
-		cells.push_back(cell);
-	}
-
-	// Store randomly generated dead and alive cell states
-	genArray(states, rows, cols);
-
-	std::vector<State> next;
-
-	// Render loop
-	while(window.isOpen())
-	{
-		sf::Event event;
-		while(window.pollEvent(event))
-		{
-			if(event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		next = states;
-
-		checkNeighbours(states, next, rows, cols);
-
-		window.clear(sf::Color(0, 0, 0, 255));
-
-		for(int i = 0; i < rows; i++)
-		for(int j = 0; j < cols; j++)
-		{
-			int currentPos = i + j * rows;
-			if(states[currentPos] == ALIVE)
-				window.draw(cells[currentPos]);
-		}
-
-		states = next;
-
-		window.display();
-	}
-
-	return 0;
-}
-
-// Generate an array of rectangle shapes with randomized values.
-void genArray(std::vector<State>& grid, int rows, int cols)
-{
-	std::srand(time(nullptr));
-
-	for(int i = 0; i < rows; i++)
-	for(int j = 0; j < cols; j++)
-		grid.push_back((State)(std::rand() % 2));
-}
-
-// Check the neighbours of a cell and add up the number of alive cells
-int checkCell(std::vector<State>& neighbours)
-{
 	int sum = 0;
-	for(unsigned int i = 0; i < neighbours.size(); i++)
+	for(int i = -1; i < 2; i++)
+	for(int k = -1; k < 2; k++)
 	{
-		if(neighbours[i] == ALIVE)
-		{
+		if(i == 0 && k == 0)
+			continue;
+
+		if(grid[index + i*cols + k])
 			sum++;
-		}
 	}
+
 	return sum;
 }
 
-// Check each neighbour to see if it will die or live in the next generation
-void checkNeighbours(std::vector<State>& grid, std::vector<State>& next, int rows, int cols)
+int main()
 {
-	for(int i = 0; i < rows; i++)
-	for(int j = 0; j < cols; j++)
-	{
-		int currentPos = i + j * cols;
+	// Seed randomness
+	srand(time(nullptr));
 
-		// Store all neighbours of each cell in a vector
-		std::vector<State> neighbours;
-		
-		for(int k = -1; k < 2; k++)
-		for(int l = -1; l < 2; l++)
+	// Constants
+	const int PIXLWIDTH = 2;
+	const int WIDTH = 1280;
+	const int HEIGHT = 720;
+	const int COLS = WIDTH / PIXLWIDTH;
+	const int ROWS = HEIGHT / PIXLWIDTH;
+
+	// Grid of cells
+	bool grid[ROWS * COLS];
+	for(int i = 0; i < COLS; i++)
+	for(int j = 0; j < ROWS; j++)
+	{
+		grid[i + j*COLS] = rand() % 2;
+	}
+
+	// Window creation
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Game of Life");
+	window.setFramerateLimit(60);
+
+	// Rectangle to be drawn
+	sf::RectangleShape cell(sf::Vector2f(PIXLWIDTH, PIXLWIDTH));
+	cell.setFillColor(sf::Color::White);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+		bool copy[ROWS * COLS];
+		for(int i = 0; i < COLS; i++)
+		for(int j = 0; j < ROWS; j++)
 		{
-			if(!(k == 0 && l == 0))
-				neighbours.push_back(grid[((i + l + rows) % rows) * cols + ((j + k + cols) % cols)]);
+			if(grid[i + j*COLS])
+			{
+				cell.setPosition(i*PIXLWIDTH, j*PIXLWIDTH);
+				window.draw(cell);
+			}
+
+			int n = countNeighbors(grid, i + j*COLS, ROWS, COLS);
+			if((n == 2 || n == 3) && grid[i + j*COLS])
+				copy[i + j*COLS] = true;
+			else if((n==3) && !grid[i + j*COLS])
+				copy[i+j*COLS] = true;
+			else
+				copy[i+j*COLS] = false;
 		}
 
-		// Check neighbours of each cell
-		int sum = checkCell(neighbours);
-		if(sum < 2 || sum > 3)
-			next[j + i * cols] = DEAD;
-		else if(sum == 3 && grid[j + i * cols] == DEAD)
-			next[j + i * cols] = ALIVE;
-	}
+		for(int i = 0; i < COLS; i++)
+		for(int j = 0; j < ROWS; j++)
+			grid[i + j*COLS] = copy[i + j*COLS];
+
+        window.display();
+    }
+
+    return 0;
 }
